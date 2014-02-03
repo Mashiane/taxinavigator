2c1b3c2f821c81047a7f7ed0b8f796488578fd5b
Mashiane <anele@mbangas.com>
2014-02-03 19:56:02 +0200

    Update 3

--- 41.js
+++ 41.js
@@ -2,5 +2,5 @@
 frmPlacesEditor.innerHTML=[  NSB.MultiInput("placesEditor", 4, "smallfield", "Province,Place Name,Latitude,Longitude", "Province,Place Name,Latitude,Longitude", "text,text,text,text", "", " style=  ", false),
   NSB.HeaderBar_jqm("placesEditorT", "Places", "Back", "arrow-l", "Save", "check", " data-theme=b style='' class=' ' "),
   NSB.ComboBox("cboProvince", "", "1,2", "", ""),
-  "<div id='cmdAddPlace' data-role='button' data-inline=true data-theme=b data-icon=plus data-iconpos=left data-mini=true data-corners='false' class=' ' style='margin:0px; overflow:visible; font-size:14px; font-family:helvetica; font-style:normal; font-weight:bold; color:; background-color:; '>Add</div>",
+  "<div id='cmdAddPlace' data-role='button' data-inline=true data-theme=b data-icon=plus data-iconpos=left data-mini=true data-corners='true' class=' ' style='margin:0px; overflow:visible; font-size:14px; font-family:helvetica; font-style:normal; font-weight:bold; color:; background-color:; '>Add</div>",
   ].join('');
