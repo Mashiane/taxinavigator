252edfd6e10ba1cd880c8e460418bcdbcf2adc9b
Mashiane <anele@mbangas.com>
2014-02-06 13:30:14 +0200

    Update 11

--- 41.js
+++ 41.js
@@ -1,4 +1,4 @@
 
-frmPlaces.innerHTML=[  NSB.List_jqm14('placesL', 'ul', 'none', '', 'b', '', '', 'N,N,N', 'style=', 'class=" "', '100%', true, false, '', 'false', 'false', '', 'false', 'false'),
+frmPlaces.innerHTML=[  NSB.List_jqm14('placesL', 'ul', 'none', '', 'b', '', '', 'N,N,N', 'style=', 'class=" "', '100%', true, false, '', 'false', 'true', 'Place Search...', 'false', 'false'),
   NSB.HeaderBar_jqm14('placesH', 'Places', 'Back', 'arrow-l', 'Add', 'plus', ' style="" class=" "'),
   ].join('');
--- 47.js
+++ 47.js
@@ -1,4 +1,4 @@
 
-frmTaxis.innerHTML=[  NSB.List_jqm14('taxisL', 'ul', 'none', '', 'b', '', '', 'N,N,N', 'style=', 'class=" "', '100%', true, false, '', 'false', 'false', '', 'false', 'false'),
+frmTaxis.innerHTML=[  NSB.List_jqm14('taxisL', 'ul', 'none', '', 'b', '', '', 'N,N,N', 'style=', 'class=" "', '100%', true, false, '', 'false', 'true', 'Taxi Search...', 'false', 'false'),
   NSB.HeaderBar_jqm14('taxisH', 'Taxis', 'Back', 'arrow-l', '', 'false', ' style="" class=" "'),
   ].join('');
